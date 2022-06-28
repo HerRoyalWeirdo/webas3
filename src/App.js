299c0addeb24af515ecf0830236e3290d57ed5e7
HerRoyalWeirdo <lalacandycanes@gmail.com>
2022-06-28 20:31:48 +1200

    the polyfills fix

--- src/App.js
+++ src/App.js
@@ -1,9 +1,89 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
 import Navigation from './components/Navigation';
+import { createGradientItem } from './api';
+
+import {Paper, Grid, Button, Typography, TextField, makeStyles} from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    marginTop: 80,
+    margin: 20,
+    flexGrow: 1
+  },
+  list: {
+    marginTop: 20
+  }
+});
 
 function App() {
+  //return <Navigation title="Broken Rainbow app" />
+
+
+  const classes = useStyles();
+  const [gradDetail, setgradDetail] = useState('');
+  const [gradients, setgrad] = useState([]);
+
+  function handlegradDetailChange(event) {
+    console.log(event.target.value);
+    setgradDetail(event.target.value);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    createGradientItem(gradDetail).then(res => {
+      console.log('Rainbow Pain details added to the database');
+    });
+    resetInputField();
+  }
+  function resetInputField() {
+    setgradDetail('');
+  }
+
+  return (
+    <>
+      <Navigation title="Broken Rainbow app" />
+      <Grid container className={classes.root} spacing={3}>
+        <Grid item xs={12} md={6}>
+          <Paper style={{ margin: 16, padding: 16 }}>
+            <Grid container>
+              <Grid xs={10} md={11} item style={{ paddingRight: 16 }}>
+                <TextField
+                  type="text"
+                  name={gradDetail}
+                  value={gradDetail}
+                  placeholder="Add your Pain here"
+                  fullWidth
+                  onChange={handlegradDetailChange}/>
+              </Grid>
+              <Grid xs={2} md={1} item>
+                <Button
+                  fullWidth
+                  color="secondary"
+                  variant="outlined"
+                  onClick={handleSubmit}>
+                  Add
+                </Button>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={5} className={classes.list}>
+          <Typography>List of Pain</Typography>
+        </Grid>
+      </Grid>
+        
+          {/* <Typography variant="p">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Reprehenderit itaque iusto perferendis consectetur culpa
+            accusantium! Dolor, nemo natus ducimus esse minus, ut laborum
+            excepturi deserunt recusandae praesentium eligendi consectetur
+            labore.
+          </Typography> */}
+       
+    </>
+  );
+
   // return (
   //   <div className="App">
   //     <header className="App-header">
@@ -22,7 +102,7 @@ function App() {
   //     </header>
   //   </div>
   // );
-  return <Navigation title="Expense Tracker app" />;
+  
 }
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,34 @@
+import { client, q } from '../config/db.js';
+// import React from 'react';
+// export const getAllExpenses;
+// export const createExpenseItem;
+// export const deleteExpenseItem;
+
+export const getAllGradients = client
+  .query(q.Paginate(q.Match(q.Ref('indexes/all_grads'))))
+  .then(response => {
+    const gradRef = response.data;
+    const getAllDataQuery = gradRef.map(ref => {
+      return q.Get(ref);
+    });
+    return client.query(getAllDataQuery).then(data => data);
+  })
+  .catch(error => console.error('Error: ', error.message));
+
+export const createGradientItem = name =>
+client
+.query(
+    q.Create(q.Collection('gradients'), {
+    data: {
+        name
+    }
+    })
+)
+.then(ret => ret)
+.catch(error => console.error('Error: ', error.message));
+
+export const deleteGradientItem = gradId =>
+  client
+    .query(q.Delete(q.Ref(q.Collection('gradients'), gradId)))
+    .then(ret => ret)
+    .catch(error => console.error('Error: ', error.message));
